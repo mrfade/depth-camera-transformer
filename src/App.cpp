9abeebc63844b1a5e37c5a91917ed89d9d46e0c3
Enes <enessolak99@gmail.com>
2019-12-13 12:55:15 +0300

    change function params int to double in app

--- src/App.cpp
+++ src/App.cpp
@@ -18,25 +18,25 @@ int main()
 	pc2 = camera2.Capture();
 
 	RadiusOutlierFilter rof;
-	rof.setRadius(25);
+	rof.setRadius(25.0);
 
 	rof.filter(pc1);
 	rof.filter(pc2);
 
 	PassThroughFilter filter1, filter2;
-	filter1.setlowerLimitX(0);
-	filter1.setupperLimitX(400);
-	filter1.setlowerLimitY(0);
-	filter1.setupperLimitY(400);
-	filter1.setlowerLimitZ(-45);
-	filter1.setupperLimitZ(45);
+	filter1.setlowerLimitX(0.0);
+	filter1.setupperLimitX(400.0);
+	filter1.setlowerLimitY(0.0);
+	filter1.setupperLimitY(400.0);
+	filter1.setlowerLimitZ(-45.0);
+	filter1.setupperLimitZ(45.0);
 	
-	filter2.setlowerLimitX(0);
-	filter2.setupperLimitX(500);
-	filter2.setlowerLimitY(0);
-	filter2.setupperLimitY(500);
-	filter2.setlowerLimitZ(-45);
-	filter2.setupperLimitZ(45);
+	filter2.setlowerLimitX(0.0);
+	filter2.setupperLimitX(500.0);
+	filter2.setlowerLimitY(0.0);
+	filter2.setupperLimitY(500.0);
+	filter2.setlowerLimitZ(-45.0);
+	filter2.setupperLimitZ(45.0);
 
 	filter1.filter(pc1);
 	filter2.filter(pc2);
